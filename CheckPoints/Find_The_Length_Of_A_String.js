4e818396a8205d34be233fdffbada57b922741f5
Milica Sucevic <milicasucevic@gmail.com>
2017-01-17 17:25:23 +0000

    Code Updated

---
+++ CheckPoints/Find_The_Length_Of_A_String.js
@@ -0,0 +1,35 @@
+/* You can find the length of a String value by writing .length after the string variable or string literal.
+
+"Alan Peter".length; // 10
+
+For example, if we created a variable var firstName = "Charles", we could find out how long the string "Charles" is by using the firstName.length property.
+
+Instructions
+Use the .length property to count the number of characters in the lastName variable and assign it to lastNameLength.
+
+lastNameLength should be equal to eight.
+You should be getting the length of lastName by using .length like this: lastName.length.
+
+Code by Milica Sucevic
+milicasucevic@gmail.com
+https://www.freecodecamp.com/millie91
+
+*/
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+var lastName = "Sucevici";
+
+lastNameLength = lastName.length;
+
+
