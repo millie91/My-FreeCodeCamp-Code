aff64bf8ffddc37d425de9141811a9895e41612f
Milica Sucevic <milicasucevic@gmail.com>
2017-01-17 17:38:09 +0000

    Code Updated

---
+++ CheckPoints/Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,28 @@
+/* Unlike strings, the entries of arrays are mutable and can be changed freely.
+
+Example
+
+var ourArray = [3,2,1];
+ourArray[0] = 1; // equals [1,2,1]
+Instructions
+Modify the data stored at index 0 of myArray to a value of 3.
+
+myArray should now be [3,2,3].
+You should be using correct index to modify the value in myArray.
+
+Code by Milica Sucevic
+milicasucevic@gmail.com
+https://www.freecodecamp.com/millie91
+
+*/
+
+// Example
+var ourArray = [1,2,3];
+ourArray[1] = 3; // ourArray now equals [1,3,3].
+
+// Setup
+var myArray = [1,2,3];
+
+// Only change code below this line.
+myArray[0] = 3;
+
---
+++ CheckPoints/Nest_One_Array_Within_Another_Array.js
@@ -0,0 +1,21 @@
+/* You can also nest arrays within other arrays, like this: [["Bulls", 23], ["White Sox", 45]]. This is also called a Multi-dimensional Array.
+
+Instructions
+Create a nested array called myArray.
+
+Run tests (ctrl + enter)
+
+myArray should have at least one array nested within another array.
+
+Code by Milica Sucevic
+milicasucevic@gmail.com
+https://www.freecodecamp.com/millie91
+
+*/
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Kahlo", 1], ["Joana", 30]];
+
